7d618abb13676da3c43f6e1fab38033303869f8b
Sy Le <lenguyensy@gmail.com>
2017-06-28 20:22:27 -0700

    basic babel build config

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,21 @@ var SRC_DIR = path.resolve(__dirname, 'src');
 var DEST_DIR = path.resolve(__dirname, 'public');
 
 var config = {
-  entry: SRC_DIR + '/index.jsx',
-  output: {
-    path: DEST_DIR,
-    filename: 'index.js'
-  }
+    entry: SRC_DIR + '/index.jsx',
+    output: {
+        path: DEST_DIR,
+        filename: 'index.js'
+    },
+    module : {
+    loaders : [
+            {
+                test : /\.jsx?/,
+                include : SRC_DIR,
+                loader : 'babel-loader'
+            }
+        ]
+    }
+
 };
 
 module.exports = config;
