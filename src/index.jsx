4f234372051960ffda8daac6fc0dcd397a525480
Sy Le <lenguyensy@gmail.com>
2017-06-28 21:59:39 -0700

    make example generation more fluid...

--- src/index.jsx
+++ src/index.jsx
@@ -14,36 +14,62 @@ const mountNode = document.getElementById('app-container');
 // examples...
 let exampleDoms = [];
 
-exampleDoms.push(
-    <div className="example-section">
-        <h2>Simple <b>BUTTONS</b> Examples</h2>
+// function to get the dom...
+const _getExampleDom = (elmTitle, elmContent) => {
+    return (
+        <div className="example-section">
+            <div className="example-section-title">{elmTitle}</div>
+            <div className="example-section-content">
+                {elmContent}
+            </div>
+        </div>
+    );
+}
 
+const _pushExampleDom = (elmTitle, elmContent) => {
+    exampleDoms.push(
+        _getExampleDom(
+            elmTitle,
+            elmContent
+        )
+    );
+}
+
+const _getExampleDomList = () => {
+    // function that handle the mapping
+    let counter = 0;
+    return exampleDoms.map( (curDom) => {
+        counter++;
+        return <div key={counter}>{curDom}</div>
+    });
+}
+
+
+// example start here
+_pushExampleDom(
+    '<ButtonToolbar /> and <Button bsStyle />',
+    (
         <ButtonToolbar>
             <Button>Default</Button>
-            <Button bsStyle="primary">Primary</Button>
-            <Button bsStyle="success">Success</Button>
-            <Button bsStyle="info">Info</Button>
-            <Button bsStyle="warning">Warning</Button>
-            <Button bsStyle="danger">Danger</Button>
-            <Button bsStyle="link">Link</Button>
+            <Button bsStyle="primary">primary</Button>
+            <Button bsStyle="success">success</Button>
+            <Button bsStyle="info">info</Button>
+            <Button bsStyle="warning">warning</Button>
+            <Button bsStyle="danger">danger</Button>
+            <Button bsStyle="link">link</Button>
         </ButtonToolbar>
-    </div>
-);
+    )
+)
 
 
 
-// function that handle the mapping
-let counter = 0;
-exampleDoms = exampleDoms.map( (curDom) => {
-    counter++;
-    return <div key={counter}>{curDom}</div>
-});
+
 
 
 //render
 ReactDom.render(
     <div>
-        { exampleDoms }
+        { _getExampleDomList() }
     </div>,
     mountNode
 );
