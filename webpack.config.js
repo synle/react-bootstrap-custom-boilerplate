0a9f026c6cfb68ababca07dafbd2014a25c4310f
Sy Le <lenguyensy@gmail.com>
2017-06-28 20:20:11 -0700

    basic build pipeline

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var SRC_DIR = path.resolve(__dirname, 'src');
+var DEST_DIR = path.resolve(__dirname, 'public');
+
+var config = {
+  entry: SRC_DIR + '/index.jsx',
+  output: {
+    path: DEST_DIR,
+    filename: 'index.js'
+  }
+};
+
+module.exports = config;
