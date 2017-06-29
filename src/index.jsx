68e280f13d6843e9af53042184d62af8e0819f6d
Sy Le <lenguyensy@gmail.com>
2017-06-28 21:52:48 -0700

    group examples in the list

--- src/index.jsx
+++ src/index.jsx
@@ -5,33 +5,45 @@ require('./index.scss');
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import { Button, ButtonToolbar } from 'react-bootstrap';
+import { Accordion, Alert, Badge, Breadcrumb, BreadcrumbItem, Button, ButtonGroup, ButtonToolbar, Carousel, CarouselItem, Checkbox, Clearfix, CloseButton, ControlLabel, Col, Collapse, Dropdown, DropdownButton, Fade, Form, FormControl, FormGroup, Glyphicon, Grid, HelpBlock, Image, InputGroup, Jumbotron, Label, ListGroup, ListGroupItem, Media, MenuItem, Modal, ModalBody, ModalFooter, ModalHeader, ModalTitle, Nav, Navbar, NavbarBrand, NavDropdown, NavItem, Overlay, OverlayTrigger, PageHeader, PageItem, Pager, Pagination, PaginationButton, Panel, PanelGroup, Popover, ProgressBar, Radio, ResponsiveEmbed, Row, SafeAnchor, SplitButton, Tab, TabContainer, TabContent, Table, TabPane, Tabs, Thumbnail, Tooltip, Well } from 'react-bootstrap';
 
 
 // mount node
 const mountNode = document.getElementById('app-container');
 
-// basically render buttons
-const buttonsInstance = (
-  <ButtonToolbar>
-    <Button>Default</Button>
-    <Button bsStyle="primary">Primary</Button>
-    <Button bsStyle="success">Success</Button>
-    <Button bsStyle="info">Info</Button>
-    <Button bsStyle="warning">Warning</Button>
-    <Button bsStyle="danger">Danger</Button>
-    <Button bsStyle="link">Link</Button>
-  </ButtonToolbar>
+// examples...
+let exampleDoms = [];
+
+exampleDoms.push(
+    <div className="example-section">
+        <h2>Simple <b>BUTTONS</b> Examples</h2>
+
+        <ButtonToolbar>
+            <Button>Default</Button>
+            <Button bsStyle="primary">Primary</Button>
+            <Button bsStyle="success">Success</Button>
+            <Button bsStyle="info">Info</Button>
+            <Button bsStyle="warning">Warning</Button>
+            <Button bsStyle="danger">Danger</Button>
+            <Button bsStyle="link">Link</Button>
+        </ButtonToolbar>
+    </div>
 );
+
+
+
+// function that handle the mapping
+let counter = 0;
+exampleDoms = exampleDoms.map( (curDom) => {
+    counter++;
+    return <div key={counter}>{curDom}</div>
+});
 
 
 //render
 ReactDom.render(
-    <div className="col-sm-12">
-        <div><h2>Simple <b>BUTTONS</b> Examples</h2></div>
-        <div>
-            { buttonsInstance }
-        </div>
+    <div>
+        { exampleDoms }
     </div>,
     mountNode
 );
