87c52d5da92d859dd137ddd0781b0126c4f23095
Sy Le <syle@linkedin.com>
2017-06-29 18:41:14 -0700

    tabs, pagination, progressbar

--- src/page-guide.jsx
+++ src/page-guide.jsx
@@ -1098,7 +1098,170 @@ _pushExampleDom(
 
 
 
+_pushExampleDom(
+    'Tabs - Simple',
+    (
+        <article>
+            <h3>Simple Tab</h3>
+                    <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
+            <Tab eventKey={1} title="Tab 1">Tab 1 content</Tab>
+            <Tab eventKey={2} title="Tab 2">Tab 2 content</Tab>
+            <Tab eventKey={3} title="Tab 3" disabled>Tab 3 content</Tab>
+          </Tabs>
+        </article>
+    ),
+    '<Tabs> <Tab></Tab> </Tabs>'
+)
 
+
+_pushExampleDom(
+    'Tabs - Complex (More Flexible)',
+    (
+        <article>
+            <Tab.Container id="tabs-with-dropdown" defaultActiveKey="first">
+                <Row className="clearfix">
+                  <Col sm={12}>
+                    <Nav bsStyle="tabs">
+                      <NavItem eventKey="first">
+                        Tab 1
+                      </NavItem>
+                      <NavItem eventKey="second">
+                        Tab 2
+                      </NavItem>
+                      <NavDropdown eventKey="3" title="Dropdown" id="nav-dropdown-within-tab">
+                        <MenuItem eventKey="3.1">Action</MenuItem>
+                        <MenuItem eventKey="3.2">Another action</MenuItem>
+                        <MenuItem eventKey="3.3">Something else here</MenuItem>
+                        <MenuItem divider />
+                        <MenuItem eventKey="3.4">Separated link</MenuItem>
+                      </NavDropdown>
+                    </Nav>
+                  </Col>
+                  <Col sm={12}>
+                    <Tab.Content animation>
+                      <Tab.Pane eventKey="first">
+                        Tab 1 content
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        Tab 2 content
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="3.1">
+                        Tab 3.1 content
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="3.2">
+                        Tab 3.2 content
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="3.3">
+                        Tab 3.3 content
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="3.4">
+                        Tab 3.4 content
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Col>
+                </Row>
+              </Tab.Container>
+        </article>
+    ),
+    '<Tabs> <Tab></Tab> </Tabs>'
+)
+
+
+
+const PaginationBasic = React.createClass({
+  getInitialState() {
+    return {
+      activePage: 1
+    };
+  },
+
+  handleSelect(eventKey) {
+    this.setState({
+      activePage: eventKey
+    });
+  },
+
+  render() {
+    return (
+      <div>
+        <Pagination
+          bsSize="large"
+          items={10}
+          activePage={this.state.activePage}
+          onSelect={this.handleSelect} />
+        <br />
+
+        <Pagination
+          bsSize="medium"
+          items={10}
+          activePage={this.state.activePage}
+          onSelect={this.handleSelect} />
+        <br />
+
+        <Pagination
+          bsSize="small"
+          items={10}
+          activePage={this.state.activePage}
+          onSelect={this.handleSelect} />
+      </div>
+    );
+  }
+});
+
+
+_pushExampleDom(
+    'Pagination',
+    (
+        <article>
+            <h3>DEFAULT</h3>
+            <div>
+                <ProgressBar bsStyle="success" now={40} />
+                <ProgressBar bsStyle="info" now={20} />
+                <ProgressBar bsStyle="warning" now={60} />
+                <ProgressBar bsStyle="danger" now={80} />
+            </div>
+
+            <h3>Striped</h3>
+            <div>
+                <ProgressBar striped bsStyle="success" now={40} />
+                <ProgressBar striped bsStyle="info" now={20} />
+                <ProgressBar striped bsStyle="warning" now={60} />
+                <ProgressBar striped bsStyle="danger" now={80} />
+            </div>
+
+
+            <h3>Animated</h3>
+            <div>
+                <ProgressBar active now={45} />
+            </div>
+
+            <h3>Stacked</h3>
+            <div>
+                <ProgressBar>
+                    <ProgressBar striped bsStyle="success" now={35} key={1} />
+                    <ProgressBar bsStyle="warning" now={20} key={2} />
+                    <ProgressBar active bsStyle="danger" now={10} key={3} />
+                </ProgressBar>
+            </div>
+        </article>
+    ),
+    '<Pagination />'
+)
+
+
+
+
+
+
+_pushExampleDom(
+    'Pagination',
+    (
+        <article>
+            <PaginationBasic />
+        </article>
+    ),
+    '<Pagination />'
+)
 
 
 
